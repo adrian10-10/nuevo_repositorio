6491abcc31db32630a847d3d7de774f3d9d0080b
1DAW <adrix_99@hotmail.es>
2024-02-08 09:55:53 +0100

    Initial commit

---
+++ NetBeansProjects/Entorno2/src/entorno2/Entorno2.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package entorno2;
+
+/**
+ *
+ * @author 1DAW
+ */
+public class Entorno2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("Hola mundo");
+ 
+                
+    }
+    
+}
